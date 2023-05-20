58b76b5ccecdeea2d9aa2c7c555ab22f649fef59
Julio Faerman <jufaerma@redhat.com>
2023-05-20 19:39:43 +0200

    wip

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World',
+        'output': 'Ola Mundo',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
